93b9b34071acf4272c11ce9141c5cb31fb72a9bf
Kaleb White <kalebw02@ad.unc.edu>
2025-07-11 00:25:55 -0400

    Change zod error messages

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,6 +1,6 @@
 import * as z from "zod"
 
 export const signInSchema = z.object({
-    email: z.email(),
-    password: z.string().min(1)
+    email: z.email("Submitted email invalid"),
+    password: z.string().min(1, "Must be at least one character")
 })
